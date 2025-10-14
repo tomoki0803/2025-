703b58cbbcc71a4379445d713ca037256e3d2413
tomoki0803 <zm25352@josai.ac.jp>
2025-10-14 09:31:50 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,12 +1,3 @@
-alert(`Hello, Osamu!`);
+alert(`Hello, Tomoki!`);
 alert(1+1);
 alert(Math.sqrt(1+1));
-
-alert(2+3);
-alert(`2`+`3`);
-alert(Number(`2`)+Number(`3`));
-alert(`2`+3);
-alert(Number(`2`)+3);
-
-
-alert(Math.sqrt(2の平方根と3の平方根の和を計算しなさい));
