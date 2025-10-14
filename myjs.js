4966518b90d17a2b646c3c1ab9b4a8b185852942
tomoki0803 <zm25352@josai.ac.jp>
2025-10-14 09:54:51 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -2,9 +2,9 @@ alert(`Hello, Tomoki!`);
 alert(1+1);
 alert(Math.sqrt(1+1));
 
-alert(2の平方根と3の平方根の和を計算しなさい。);
-alert(1を0で割るとどうなるか);
-alert(0を0で割るとどうなるか);
+alert('2の平方根と3の平方根の和を計算しなさい。');
+alert('1を0で割るとどうなるか');
+alert('0を0で割るとどうなるか');
 
 alert(2+3);
 alert('2'+'3');
