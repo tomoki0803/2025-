0e0bda580f46bfccd63f5be8c2caed7ff41473cf
tomoki0803 <zm25352@josai.ac.jp>
2025-10-28 10:01:10 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,4 +1,4 @@
 var who65=`world`;
-function sayhello65() {alert(`Hello,`+who65+`!`);}
-function65 taro65() {who65=`太郎`;}
-function65 hanako() {who65=`花子`;}
+function sayhello65(){alert(`Hello,`+who65+`!`);}
+function65 taro65(){who65=`太郎`;}
+function65 hanako(){who65=`花子`;}
