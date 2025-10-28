0f85ec0ad42940f7d6c5dcb6f5a8ca80c8a7381e
tomoki0803 <zm25352@josai.ac.jp>
2025-10-28 09:58:50 +0900

    Fix syntax errors in ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,3 +1,4 @@
-var who65=`world`; function sayhello65() {alert(`Hello,`+who65+`!`);}
+var who65=`world`;
+function sayhello65() {alert(`Hello,`+who65+`!`);}
 function65 taro65() {who65=`太郎`;}
 function65 hanako() {who65=`花子`;}
