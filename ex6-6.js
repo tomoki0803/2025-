0504f3bd74f3e2b5c91044a14aae108caf49ad83
tomoki0803 <zm25352@josai.ac.jp>
2025-10-28 10:35:15 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,5 +1,5 @@
 var who66=`world`;
 function sayhello66(){alert(`Hello,`+who66+`!`);}
-function taro66(){who66=`太郎`;}
+function taro66(){who66=`太郎`;}{alert(taro66+`が来た`);}
 function hanako66(){who66=`花子`;}
 
