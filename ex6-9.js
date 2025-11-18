9ef837ec7f06271f8be9d3eaf652f59a5b10ec58
tomoki0803 <zm25352@josai.ac.jp>
2025-11-18 11:18:22 +0900

    Update ex6-9.js

--- ex6-9.js
+++ ex6-9.js
@@ -1,4 +1,4 @@
-function sayhello69(){alert(`Hello,`+document.getElementById(`who69`).innerHTML+`!`);
-function taro69(){document.getElementById(`who69`).innerHTML=`太郎`;}
-function hanako69(){document.getElementById(`who69`).innerHTML=`花子`;}             
+function sayhello69(){alert('Hello,'+document.getElementById('who69').innerHTML+'!');
+function taro69(){document.getElementById('who69').innerHTML='太郎';}
+function hanako69(){document.getElementById('who69').innerHTML='花子';}             
                       
