61d56460ae3f5e766f63ca1aa1d78416a570ed73
tomoki0803 <zm25352@josai.ac.jp>
2025-11-25 09:45:18 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,2 +1,2 @@
-function gazo611()
-{document.getElementById('who611').innerHTML='<img src="./image/image5.png"alt="オン" width="200">';} 
+function gazo612()
+{document.getElementById('who612').innerHTML='<img src="./image/image5.png"alt="オン" width="200">';} 
