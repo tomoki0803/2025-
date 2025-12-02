d6e861c474693abf6e5e3f0952560561e70f590a
tomoki0803 <zm25352@josai.ac.jp>
2025-12-02 11:10:24 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function changephoto(){const img=document.getElementById("my-photo");img.src="image/koki2.png";}
+function changephoto(){const img=document.getElementById("my-photo");img.src="image/koki2.jpg";}
 function resetphoto(){const img=document.getElementById("my-photo");img.src="image/koki1.jpg";}
