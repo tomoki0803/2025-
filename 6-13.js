16b725fdbf3e78a6c592a798e9cf858a16f1ef4d
tomoki0803 <zm25352@josai.ac.jp>
2025-12-02 09:55:07 +0900

    Update 6-13.js

--- 6-13.js
+++ 6-13.js
@@ -1,1 +1,5 @@
+function imagechange1()
+{document.getElementById('who613').innerHTML='<img src="./image/image5.png"alt="obake gazo">';} 
 
+function imagechange2()
+{document.getElementById('who612').innerHTML='<img src="./image/image6.jpg"alt="panda gazo">';}
