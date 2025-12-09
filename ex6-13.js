687eea27efaff971f91371c4329c26ba748d855f
tomoki0803 <zm25352@josai.ac.jp>
2025-12-09 10:42:03 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -2,4 +2,4 @@ function imagechange1()
 {document.getElementById('who613').innerHTML='<img src="./image/ju-logo.jpg"alt="オフ"　class="imagechange">';} 
 
 function imagechange2()
-{document.getElementById('who613').innerHTML='<img src="./image/image6.jpg"alt="オン" class="imagechange">';}
+{document.getElementById('who613').innerHTML='<img src="./image/image/keiei_small (1).jpg"alt="オン" class="imagechange">';}
