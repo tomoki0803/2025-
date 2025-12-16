05ff65b5d9bcdd98fe21b960cab0c69e020dba86
tomoki0803 <zm25352@josai.ac.jp>
2025-12-16 10:42:06 +0900

    Update ex6-18.js

--- ex6-18.js
+++ ex6-18.js
@@ -2,7 +2,7 @@
 let isChanged=false;
 function switchImage18(){const img=document.getElementById("pic18");
 //状況に応じて画像を切り替え
-if(!isChanged){img.src="image5.png"; isChanged=true;}
-          else{img.src="image6.jpg"; isChanged=false;}
+if(!isChanged){img.src="./image/image5.png"; isChanged=true;}
+          else{img.src="./image/image6.jpg"; isChanged=false;}
 //3秒後に再びこの関数を呼び戻して元に戻す
 setTimeout("switchImage18()",3000);}                         
