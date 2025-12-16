f6dec01a1b5493af8fa37d3484a8accd653afcfc
tomoki0803 <zm25352@josai.ac.jp>
2025-12-16 10:25:33 +0900

    Create ex6-18.js

---
+++ ex6-18.js
@@ -0,0 +1,8 @@
+//状態管理用の変数
+let isChanged="false;
+function switchIMage18(){const img=document.getElementById("pic18");
+//状況に大路て画像を切り替え
+if(!isChanged){img.src="image5.png"; isChanged=true;}
+              {img.src="image6.jpg"; isChanged=false;}
+//3秒後に再びこの関数を呼び戻して元に戻す
+setTimeout("switchImage18()",3000);}                         
