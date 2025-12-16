6b44cc0d25ba6fc48daf43ab69f02b053d323b94
tomoki0803 <zm25352@josai.ac.jp>
2025-12-16 11:25:52 +0900

    Add class to image in imagechange1 function

--- ex6-13.js
+++ ex6-13.js
@@ -1,5 +1,5 @@
 function imagechange1()
-{document.getElementById('who613').innerHTML='<img src="./image/ju-logo.jpg" alt="オフ"　width="500" height="200">';} 
+{document.getElementById('who613').innerHTML='<img src="./image/ju-logo.jpg" alt="オフ"　class="imagechange">';} 
 
 function imagechange2()
 {document.getElementById('who613').innerHTML='<img src="./image/image6.jpg" alt="オン" class="imagechange">';}
